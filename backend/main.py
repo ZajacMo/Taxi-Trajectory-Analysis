4fe4c933bafe461a5f0b97f20593cb849d7f62c4
Zajac Mo <zonga_mo@foxmail.com>
2025-05-17 21:57:00 +0800

    feat: 添加后端API接口及功能模块
    - 新增`main.py`作为后端入口，集成多个功能模块的API接口
    - 添加`F1.py`、`F3.py`、`F4.py`、`F56.py`、`F7.py`、`F8.py`、`F9.py`等模块，分别实现轨迹查询、区域查询、热力图数据、流量分析、频繁路径分析等功能
    - 每个模块提供对应的Flask API接口，支持GET和POST请求

---
+++ backend/main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify
+from F1 import get_trail_lists,get_trails_post
+from F3 import query_region
+from F4 import get_heatmap_data
+from F56 import analyze_flow
+from F7 import frequent_paths
+from F8 import frequent_paths_ab
+from F9 import analyze_shortest_path
+from F56 import analyze_flow
+
+app = Flask(__name__)
+
+# F1
+@app.route('/trailLists', methods=['GET'])
+def new_get_trail_lists():
+    return get_trail_lists()
+
+@app.route('/trails/data', methods=['POST'])
+def new_get_trails_post():
+    return get_trails_post()
+
+# F3
+@app.route('/query_region', methods=['POST'])
+def new_query_region():
+    return query_region()
+
+
+# F4
+@app.route('/heatmap', methods=['GET'])
+def new_get_heatmap_data():
+    return get_heatmap_data()
+
+# F56
+@app.route('/flow_analysis', methods=['GET'])
+def new_analyze_flow():
+    return analyze_flow()
+
+# F7
+@app.route('/frequent_paths', methods=['POST'])
+def new_frequent_paths():
+    return frequent_paths()
+
+# F8
+@app.route('/frequent_paths_ab', methods=['POST'])
+def new_frequent_paths_ab():
+    return frequent_paths_ab()
+
+# F9
+@app.route('/optimized_path', methods=['GET'])
+def new_analyze_shortest_path():
+    return analyze_shortest_path()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
